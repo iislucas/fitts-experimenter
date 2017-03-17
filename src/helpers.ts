0344d91969e348ff6f7c05533b860232e0ae6f84
Lucas Dixon <ldixon@google.com>
2017-03-17 01:24:17 -0400

    fix date recording (used to be: year, month (jan=0), day of week (0=sunday))

--- src/helpers.ts
+++ src/helpers.ts
@@ -188,7 +188,7 @@ export class OrbitingCircleSprite {
 
 export function dateStringOfTimestamp(timestamp:number) : string {
   let date = new Date(timestamp);
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}` +
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}` +
          `-${date.getHours()}.${date.getMinutes()}.${date.getSeconds()}.` +
          `${date.getMilliseconds()}`;
 }
