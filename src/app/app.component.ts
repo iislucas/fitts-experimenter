54210c8b10b2e3e3cbeb6f9ae09bbca7500b549e
Lucas Dixon <ldixon@google.com>
2017-04-02 19:08:35 -0400

    added support for vis grouping, and vis prefs saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,28 @@ import * as params from './lib/params';
 import * as trial from './lib/trial';
 import * as helpers from './lib/helpers';
 
-const STORAGE_KEY_PARAMS = 'params';
 const STORAGE_KEY_LOGS = 'logs';
+const STORAGE_KEY_PARAMS = 'params';
+const STORAGE_KEY_PREFS = 'prefs';
+
+const TAB_INDEX_RUN_TRIAL = 0;
+const TAB_INDEX_TRIALS = 1;
+const TAB_INDEX_VIZ = 2;
+
+interface VisualizationPreferences {
+  trialSearch: string;
+  trialGrouping: string;
+}
+
+const DEFAULT_PREFS : VisualizationPreferences = {
+  trialSearch: '.*',
+  trialGrouping: '.*',
+};
+
+interface SelectedTrial {
+  name: string;
+  trials: trial.Log[]
+}
 
 @Component({
   selector: 'app-root',
@@ -17,34 +37,60 @@ const STORAGE_KEY_LOGS = 'logs';
 export class AppComponent {
   title = 'Fitts Experimenter';
   trialParamsString: string;
-  logs: trial.Log[] = [];
+  trialSearch: string = '.*';
+  trialGrouping: string = '.*';
+  allTrials: trial.Log[] = [];
+  selectedTrials: SelectedTrial[] = [];
   @ViewChild('fileEl') fileEl:ElementRef;
+  @ViewChild('downloadLinkEl') downloadLinkEl:ElementRef;
   @ViewChild('tabBar') tabsGroup: MdTabGroup;
   @ViewChild('sidenav') sidenav: MdSidenav;
 
   ngOnInit() {
-    this.loadParams();
-    this.logs = JSON.parse(localStorage.getItem(STORAGE_KEY_LOGS));
+    this.restoreVisualizationPrefs();
+    this.restoreParams();
+    this.restoreTrials();
   }
 
-  dateStringifyLog(log: trial.Log) : string {
+  dateStringifyTrialLog(log: trial.Log) : string {
     let length = Math.round((log.end_timestamp - log.start_timestamp) / 1000);
     let date = new Date(log.start_timestamp);
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}` +
          `@${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}+${length}s`;
   }
 
+  // Visualization Preferences
+  saveVisualizationPrefs() {
+    localStorage.setItem(STORAGE_KEY_PREFS, JSON.stringify({
+      trialSearch: this.trialSearch,
+      trialGrouping: this.trialGrouping,
+    }));
+  }
+  restoreVisualizationPrefs() {
+    let prefsString = localStorage.getItem(STORAGE_KEY_PREFS);
+    if(!prefsString) {
+      this.resetVisualizationPrefs();
+      return;
+    }
+    let prefs: VisualizationPreferences = JSON.parse(prefsString);
+    this.trialSearch = prefs.trialSearch;
+    this.trialGrouping = prefs.trialGrouping;
+  }
+  resetVisualizationPrefs() {
+    this.trialSearch = DEFAULT_PREFS.trialSearch;
+    this.trialGrouping = DEFAULT_PREFS.trialGrouping;
+  }
+
+  // Default Trial Params
   saveParams() {
     localStorage.setItem(STORAGE_KEY_PARAMS, this.trialParamsString);
   }
-
-  loadParams() {
+  restoreParams() {
     this.trialParamsString = localStorage.getItem(STORAGE_KEY_PARAMS);
     if(!this.trialParamsString) {
       this.resetParams();
     }
   }
-
   resetParams() {
     this.trialParamsString = JSON.stringify(params.defaults, null, 2);
   }
@@ -53,31 +99,42 @@ export class AppComponent {
     let trialParams: params.Experiment = JSON.parse(this.trialParamsString);
   }
 
-  saveFile() {
+  saveTrialsToFile() {
     let file = new File(
-      [JSON.stringify(this.logs)],
+      [JSON.stringify(this.allTrials)],
       'fitts-app-' + helpers.dateStringOfTimestamp(Date.now()) + '.json',
       { type: 'application/octet-stream;charset=utf-8'}
     );
     // let data = new Blob([experiment.textOfLogs()],
     //     { type: 'text/csv;charset=utf-8'});
     let url = URL.createObjectURL(file);
-    window.open(url);
-    this.sidenav.close();
+    this.downloadLinkEl.nativeElement.href = url;
+    this.downloadLinkEl.nativeElement.download = file.name;
+    this.downloadLinkEl.nativeElement.click();
+    // window.open(url);
+    this.sidenav.close().then(() => { URL.revokeObjectURL(url); });
   }
 
-  loadFile() {
+  // When UI wants us to remove a trial.
+  removeOneTrial(index) {
+    this.allTrials.splice(index, 1);
+  }
+
+  // Load/save of trials.
+  loadTrialsFromFile() {
     this.fileEl.nativeElement.click();
   }
-
-  removeOneTrial(index) {
-    this.logs.splice(index, 1);
-  }
-
-  clearTrials() {
-    this.logs = [];
+  deleteAllTrials() {
+    this.allTrials = [];
     this.sidenav.close();
-    this.tabsGroup.selectedIndex = 1;
+  }
+  saveTrials() {
+    localStorage.setItem(STORAGE_KEY_LOGS, JSON.stringify(this.allTrials));
+    this.sidenav.close();
+  }
+  restoreTrials() {
+    this.allTrials = JSON.parse(localStorage.getItem(STORAGE_KEY_LOGS));
+    this.sidenav.close();
   }
 
   fileSelected(event: Event) {
@@ -109,9 +166,9 @@ export class AppComponent {
         } else if (filekind === 'json') {
           console.log('parsing json');
           let moreLogs : trial.Log[] = JSON.parse((e.target as FileReader).result);
-          this.logs = moreLogs.concat(this.logs);
+          this.allTrials = moreLogs.concat(this.allTrials);
           let combinedLogs : {[id:string]: trial.Log} = {} ;
-          for (let l of this.logs) {
+          for (let l of this.allTrials) {
             combinedLogs[l.trialId] = l;
           }
           for (let l of moreLogs) {
@@ -121,10 +178,10 @@ export class AppComponent {
           for (let k of Object.keys(combinedLogs)) {
             combinedLogsList.push(combinedLogs[k]);
           }
-          
-          this.logs = combinedLogsList.sort((log) => log.start_timestamp);
-          localStorage.setItem(STORAGE_KEY_LOGS, JSON.stringify(this.logs));
-          console.log(this.logs);
+
+          this.allTrials = combinedLogsList.sort((log) => log.start_timestamp);
+          localStorage.setItem(STORAGE_KEY_LOGS, JSON.stringify(this.allTrials));
+          console.log(this.allTrials);
         }
       };
 
@@ -133,6 +190,41 @@ export class AppComponent {
     }
     files = [];
     this.sidenav.close();
-    this.tabsGroup.selectedIndex = 1;
+    this.tabsGroup.selectedIndex = TAB_INDEX_TRIALS;
+  }
+
+  showVizualizations() {
+    let trialSearchRegExp = new RegExp(this.trialSearch);
+    let trialGroupingRegExp = new RegExp(this.trialGrouping);
+
+    let trialGroups : { [name:string] : trial.Log[] } = {};
+
+    for (let t of this.allTrials) {
+      let tagStringToMatch : string = t.tags || '';
+      if (trialSearchRegExp.test(tagStringToMatch)){
+        let groupResult = trialGroupingRegExp.exec(tagStringToMatch);
+        let key = '';
+        if (groupResult) {
+          for (let s of groupResult.pop()) {
+            key = key.concat(s);
+          }
+        } else {
+          key = 'id:' + t.trialId;
+        }
+        if(!(key in trialGroups)) {
+          trialGroups[key] = [];
+        }
+        trialGroups[key].push(t);
+      }
+    }
+
+    //
+    this.selectedTrials = [];
+    for (let key of Object.keys(trialGroups)) {
+      this.selectedTrials.push({
+        name: key,
+        trials: trialGroups[key]
+      })
+    }
   }
 }
